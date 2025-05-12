f1aef69adf14af1c6c243ec670ff8685d571d737
Sankrita05 <sankritapatel2@gmail.com>
2025-05-13 02:35:17 +0530

    host change

--- plant_disease/settings.py
+++ plant_disease/settings.py
@@ -11,8 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------------
 # WARNING: Replace the secret key in production with a secure one!
 SECRET_KEY = 'django-insecure-+_=l+6ho+dt$stsh_4lzb=@y(n=f2-k*kyql301-5bxvpzyn85'
-DEBUG = True  # Disable DEBUG in production
-ALLOWED_HOSTS = []  # Add production domain(s) here
+DEBUG = False  # Disable DEBUG in production
+ALLOWED_HOSTS = ['3.78.116.192']  # Add production domain(s) here
 
 # -------------------------------
 # APPLICATION CONFIGURATION
